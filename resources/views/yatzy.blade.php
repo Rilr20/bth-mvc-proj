183ee3f07116f3c9bc47cb3321a60d7f77621dc7
Rilr20 <nrrl99@gmail.com>
2021-04-21 18:16:52 +0200

    yatzy view for yatzy game

---
+++ resources/views/yatzy.blade.php
@@ -0,0 +1,52 @@
+@extends('layout/main')
+@section('title', $title ?? "no title")
+@php
+    $tabledata = $tabledata ?? null;
+    $computerDice = $computerDice ?? null;
+    $playerDice = $playerDice ?? [];
+    $throws = $_SESSION["throws"] ?? null;
+    $throws = 3 - $throws;
+@endphp
+@section('content')
+<h1>{{$header}}</h1>
+<p>{{$message}}</p>
+<p>Yatzy som fan</p>
+
+<form action="{{ url('/yatzy') }}" method="POST">
+    @csrf
+    <div class="yatzy-box">
+        <table>
+            <tr>
+                <th></th>
+                <th>Player</th>
+                <th>Computer</th>
+            </tr>
+            {!! html_entity_decode($tabledata) !!}
+        </table>
+    </div>
+    <div class="dice-box player">
+        @foreach ($playerDice as $dice)
+            {!! html_entity_decode($dice) !!}
+            {!! html_entity_decode( "<input type='checkbox' name='chosenDice[]' value='" .  substr_count($dice, "<span class='dot'></span>") . "'>") !!}
+            {!! html_entity_decode( "<input type='hidden' name='Dice[]' value=" .  substr_count($dice, "<span class='dot'></span>") . "'>") !!}
+            {{-- @@php
+                echo "<input type='checkbox' name='chosenDice[]' value='" .  substr_count($die, "<span class='dot'></span>") . "'>";
+                echo "<input type='hidden' name='Dice[]' value=" .  substr_count($die, "<span class='dot'></span>") . "'>";
+            @endphp --}}
+        @endforeach        
+
+    </div>
+    <div class="dice-box">
+        {{ $computerDice }}
+    </div>
+    <h1> {{$throws}} throws left</h1>
+
+    <button class="game-button" value="roll" name="gameaction">Throw Dice</button>
+
+    @if ($playerDice != null)
+        <button class="game-button" value="reroll" name="gameaction">Reroll Dice</button>
+        <button class="game-button" value="confirm" name="gameaction">Add to array</button>
+    @endif
+</form>
+
+@stop
