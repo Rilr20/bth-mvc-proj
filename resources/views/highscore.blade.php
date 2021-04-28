782884e3b68f94a55e9b495e3a2e71340caf514a
Rilr20 <nrrl99@gmail.com>
2021-04-28 11:07:54 +0200

    changes to highscore view

--- resources/views/highscore.blade.php
+++ resources/views/highscore.blade.php
@@ -1,17 +1,20 @@
 @extends('layout.main')
 
 @section('content')
-<h1>Highscore Listan</h1>
+<h1 class="center">Yatzy Highscore</h1>
 <div class="book-items">
     
     <table class="highscores">
         <tr>
-            <th>Username</th>
-            <th>Score</th>
-            <th>Achieved</th>
+            <th></th>
+            <th>Användarnamn</th>
+            <th>Poäng</th>
+            <th>Datum</th>
         </tr>
     @foreach ($highscore as $hs)
+        
         <tr>
+            <td>{{$loop->iteration}}</td>
             <td>{{$hs->username}}</td>
             <td>{{$hs->score}}</td>
             <td>{{$hs->achieved}}</td>
