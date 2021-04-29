00421d7dbb0807fc4a6e6f4b54f9090d19a9b01e
Rilr20 <nrrl99@gmail.com>
2021-04-29 15:17:08 +0200

    style edited on yatzy, highscore and book page and added a responsive design for smaller devices

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-small.css') }}">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>Document - @yield('title')</title>
 
